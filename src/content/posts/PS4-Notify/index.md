4c9b2e31d157ea33d03714ca2071c4b51e3f0828
OSM-Made <oldskoolmodz@gmail.com>
2025-11-27 22:24:45 -0700

    set up giscus

--- src/content/posts/PS4-Notify/index.md
+++ src/content/posts/PS4-Notify/index.md
@@ -6,3 +6,6 @@ tags: [ 'PS4', 'Reverse Engineering']
 toc: true
 ---
 
+For years PS4 homebrew was using the rigid notify API’s exposed by Sony which didn’t allow for cool things like a custom icon. Something that kept nagging at me was the Spotify app since it had the unique ability to show the title of the song and the album art in the notify pop up. How was a third-party application able to feed a custom image to this highly restricted system? I failed to find this feature inside the Spotify application itself. This wasn't a feature exposed by Sony's public APIs.
+
+**The black box was now my target.**
--- src/site.config.ts
+++ src/site.config.ts
@@ -172,10 +172,10 @@ const config: SiteConfig = {
   // IMPORTANT: Update giscus.json in the root of the project with your own website URL
   // If you don't want to use Giscus, set this to undefined.
   giscus: {
-    repo: 'stelcodes/multiterm-astro',
-    repoId: 'R_kgDOPNnBig',
-    category: 'Giscus',
-    categoryId: 'DIC_kwDOPNnBis4CteOc',
+    repo: 'OSM-Made/multiterm-astro',
+    repoId: 'R_kgDOQeX74A',
+    category: 'General',
+    categoryId: 'DIC_kwDOQeX74M4CzJIM',
     reactionsEnabled: true, // Enable reactions on post itself
   },
   // These are characters available for the character chat feature.
